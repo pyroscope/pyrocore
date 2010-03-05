850dc6dc13b63177f058c0591cf5658d67a9871c
pyroscope <pyroscope.project@gmail.com>
2010-03-05 21:50:09 +0000

    some inline docs

--- src/tests/fifotest.sh
+++ src/tests/fifotest.sh
@@ -1,24 +1,36 @@
 #! /bin/bash
-cat $0; echo; echo "~~~ Start of test run ~~~"
-cd $(dirname $(dirname $0))
+#
+# CONCURRENT HASHING TEST SCRIPT
+#
 
+# Do a self-referential listing for piping to pastee
+cat $0; echo; echo "~~~ Start of test run ~~~"
+
+# Preparation
+cd $(dirname $(dirname $0))
 test ! -f fifotest.torrent || rm fifotest.torrent
 test ! -e fifotest.fifo || rm fifotest.fifo
 mkfifo fifotest.fifo
 
+# Start hashing process
 mktor -r incremental -o fifotest.torrent fifotest.fifo announce -v &
 
+# Start filename emitting process
 ( for file in $(find tests/ -name "*.py"); do
     echo >&2 "$file is complete!"
     echo $file
     sleep .25
 done ) >fifotest.fifo &
- 
+
+# Wait for hashing to complete
 while test ! -f fifotest.torrent; do
     echo "Waiting for metafile..."
     sleep .1
 done
 echo
+
+# Show the result
 lstor -q fifotest.torrent
 
+# Clean up
 rm fifotest.*
