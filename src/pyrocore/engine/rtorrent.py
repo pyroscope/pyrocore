5bc752b41709a43e3dbedbb5143bce52bb2cc6bc
pyroscope <pyroscope.project@gmail.com>
2010-03-08 21:48:01 +0000

    load rtorrent.rc on demand only

--- src/pyrocore/engine/rtorrent.py
+++ src/pyrocore/engine/rtorrent.py
@@ -45,17 +45,18 @@ class RtorrentEngine(base.TorrentEngine):
         self._session_dir = None
         self._download_dir = None
 
-        self._load_rtorrent_rc(config, getattr(config, "rtorrent_rc", None))
-        if config.scgi_local.startswith("/"):
-            config.scgi_local = "scgi://" + config.scgi_local
-
 
     def open(self):
         """ Open connection.
         """
+        # Only connect once
         if self._rpc is not None:
             return self._rpc
-        
+
+        # Get connection URL from rtorrent.rc
+        self._load_rtorrent_rc(config)
+
+        # Connect and get instance ID (also ensures we're connectable)
         self._rpc = xmlrpc2scgi.RTorrentXMLRPCClient(config.scgi_local)
         try:
             self.engine_id = self._rpc.get_name()
@@ -64,6 +65,7 @@ class RtorrentEngine(base.TorrentEngine):
         except Exception, exc:
             raise error.LoggableError("Can't connect to %s (%s)" % (config.scgi_local, exc))
 
+        # Get other manifest values
         self.engine_software = "rTorrent %s/%s" % (
             self._rpc.system.client_version(), self._rpc.system.library_version(),
         )
@@ -72,48 +74,63 @@ class RtorrentEngine(base.TorrentEngine):
         self._session_dir = self._rpc.get_session()
         self._download_dir = os.path.expanduser(self._rpc.get_directory())
 
+        # Return connection
         return self._rpc
 
 
-    def _load_rtorrent_rc(self, namespace, rtorrent_rc):
+    def _load_rtorrent_rc(self, namespace, rtorrent_rc=None):
         """ Load file given in "rtorrent_rc".
         """
-        self.log.debug("Loading rtorrent config from %r" % (rtorrent_rc,))
+        # Only load when needed (also prevents multiple loading)
+        if not all(hasattr(namespace, key) for key in self.RTORRENT_RC_KEYS):
+            # Get and check config file name
+            if not rtorrent_rc:
+                rtorrent_rc = getattr(config, "rtorrent_rc", None)
+            if not rtorrent_rc:
+                raise error.UserError("No 'rtorrent_rc' path defined in configuration!")
+            if not os.path.isfile(rtorrent_rc):
+                raise error.UserError("Config file %r doesn't exist!" % (rtorrent_rc,))
 
-        # Allow use of command line tools like mktor without a working rtorrent config
-        if not (rtorrent_rc and os.path.isfile(rtorrent_rc)):
-            return
+            # Parse the file
+            self.log.debug("Loading rtorrent config from %r" % (rtorrent_rc,))
+            with closing(open(rtorrent_rc)) as handle:
+                for line in handle.readlines():
+                    # Skip comments and empty lines
+                    line = line.strip()
+                    if not line or line.startswith("#"):
+                        continue
 
-        # Parse the file
-        with closing(open(rtorrent_rc)) as handle:
-            for line in handle.readlines():
-                # Skip comments and empty lines
-                line = line.strip()
-                if not line or line.startswith("#"):
-                    continue
+                    # Be lenient about errors, after all it's not our own config file
+                    try:
+                        key, val = line.split("=", 1)
+                    except ValueError:
+                        self.log.warning("Ignored invalid line %r in %r!" % (line, rtorrent_rc))
+                        continue
+                    key, val = key.strip(), val.strip()
 
-                # Be lenient about errors, after all it's not our own config file
-                try:
-                    key, val = line.split("=", 1)
-                except ValueError:
-                    self.log.warning("Ignored invalid line %r in %r!" % (line, rtorrent_rc))
-                    continue
-                key, val = key.strip(), val.strip()
+                    # Copy values we're interested in
+                    if key in self.RTORRENT_RC_KEYS and not hasattr(namespace, key):
+                        self.log.debug("Copied from rtorrent.rc: %s = %s" % (key, val))
+                        setattr(namespace, key, val)
 
-                # Copy values we're interested in
-                if key in self.RTORRENT_RC_KEYS and not hasattr(namespace, key):
-                    self.log.debug("Copied from rtorrent.rc: %s = %s" % (key, val))
-                    setattr(namespace, key, load_config.validate(key, val))
+        # Validate fields
+        for key in self.RTORRENT_RC_KEYS:
+            setattr(namespace, key, load_config.validate(key, getattr(namespace, key)))
+        if config.scgi_local.startswith("/"):
+            config.scgi_local = "scgi://" + config.scgi_local
 
 
     def __repr__(self):
         """ Return a representation of internal state.
         """
         if self._rpc:
+            # Connected state
             return "%s connected to %s [%s] via %r" % (
                 self.__class__.__name__, self.engine_id, self.engine_software, config.scgi_local,
             )
         else:
+            # Unconnected state
+            self._load_rtorrent_rc(config)
             return "%s connectable via %r" % (
                 self.__class__.__name__, config.scgi_local,
             )
