3490ba0e4bf7faaf9d3e9e8736fdfe97676d905d
pyroscope <pyroscope.project@gmail.com>
2010-09-04 15:27:19 +0000

    TODO: -H option

--- src/pyrocore/scripts/chtor.py
+++ src/pyrocore/scripts/chtor.py
@@ -58,6 +58,9 @@ class MetafileChanger(ScriptBaseWithConfig):
             help="remove all non-standard data from metafile including the info dict")
         self.add_bool_option("-R", "--clean-rtorrent",
             help="remove all rTorrent session data from metafile")
+        # TODO: -H option
+        #self.add_bool_option("-H", "--hashed", "--fast-resume",
+        #    help="add libtorrent fast-resume information")
         # TODO: chtor --tracker
         ##self.add_value_option("-T", "--tracker", "DOMAIN",
         ##    help="filter given torrents for a tracker domain")
