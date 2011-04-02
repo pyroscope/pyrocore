528e052a77f05dd65fb39346b7ddb6353dae7020
pyroscope <pyroscope.project@gmail.com>
2011-04-02 14:41:10 +0000

    bootstrap into existing virtualenv

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,12 @@ if test ! -f ../bin/activate; then
 fi
 . ../bin/activate || return 1
 
+# essential tools
+which paver >/dev/null || easy_install -U "paver>=1.0.1" || return 1
+
+# pyrobase
+test ! -d ../pyrobase || ( cd ../pyrobase && paver develop -U)
+
 # project
 paver develop -U || return 1
 paver bootstrap || return 1
