0fc1c01f0ae39e0e04333f753d89103471ed39c1
pyroscope <pyroscope.project@gmail.com>
2011-04-09 14:06:41 +0000

    working text display of up/down data

--- src/pyrocore/screenlet/rtorrent.py
+++ src/pyrocore/screenlet/rtorrent.py
@@ -1,5 +1,27 @@
-import time
-import datetime
+# -*- coding: utf-8 -*-
+""" PyroCore - RTorrent Screenlet.
+
+    Copyright (c) 2011 The PyroScope Project <pyroscope.project@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+from __future__ import with_statement
+
+import os
+import socket
+from contextlib import contextmanager
 
 #import gtk
 #import cairo
@@ -8,6 +30,95 @@ import gobject
 
 import screenlets
 from screenlets import options as opt
+
+from pyrocore.util import xmlrpc2scgi
+from pyrocore.torrent import formatting
+
+
+# TODO: Add up/down meter (graphed)
+# TODO: Number of items in views (incomplete / stopped / etc)
+# TODO: (Number of) Tracker messages
+# TODO: Events (completed, loaded, etc), possibly using libnotify / growl
+# TODO: Log displays (expandable, show last line or last update time only, by default)
+
+class RtorrentState(object):
+    """ Representation of current RTorrent state.
+    """
+
+    TEMPL = '\n'.join(["{{#}}"
+        "rT {{d.version}} - {{d.engine_id}} - up {{d.uptime | duration | strip}}",
+        "",
+        u"U: {{d.up_rate | sz}} / {{d.up_throttle | sz}} \u03a3{{d.up_size | sz}}",
+        u"D: {{d.down_rate | sz}} / {{d.down_throttle | sz}} \u03a3{{d.down_size | sz}}",
+        "",
+        "E: {{d.error}}",
+    ])
+
+
+    def __init__(self, scgi_url):
+        self.scgi_url = scgi_url
+        self._proxy = xmlrpc2scgi.RTorrentProxy(self.scgi_url)
+        self.error = ""
+
+        self.time           = 0
+        self.uptime         = 0
+        self.startup        = 0
+        self.down_size      = 0
+        self.down_rate      = 0
+        self.down_throttle  = 0
+        self.up_size        = 0
+        self.up_rate        = 0
+        self.up_throttle    = 0
+        
+        with self.transaction() as proxy:
+            self.engine_id = proxy.get_name()
+            self.version = "%s/%s" % (
+                proxy.system.client_version(), 
+                proxy.system.library_version(),
+            )
+            self.time = proxy.system.time_seconds()
+
+            try:
+                self.startup = os.path.getmtime(os.path.join(proxy.get_session().rstrip(os.sep), "rtorrent.lock"))
+            except EnvironmentError, exc:
+                print "Can't get start time: %s" % exc
+
+
+    @contextmanager
+    def transaction(self):
+        """ Do a safe transaction with the client.
+        """
+        try:
+            yield self._proxy
+        except socket.error, exc:
+            self.error = "Transaction error with %s: %s" % (self.scgi_url, exc)
+        except Exception, exc:
+            self.error = "Transaction error with %s: %s" % (self.scgi_url, exc)
+
+
+    def update(self):
+        """ Do a minor update on values that are cheap to get.
+        """
+        with self.transaction() as proxy:
+            self.time           = proxy.system.time_seconds()
+            self.down_size      = proxy.get_down_total()
+            self.down_rate      = proxy.get_down_rate()
+            self.down_throttle  = proxy.get_download_rate()
+            self.up_size        = proxy.get_up_total()
+            self.up_rate        = proxy.get_up_rate()
+            self.up_throttle    = proxy.get_upload_rate()
+            
+            self.error = ""
+
+        if self.startup:
+            self.uptime = self.time - self.startup
+
+
+    def poll(self):
+        """ Do a full poll of all download items and their state.
+        """
+        self.update()
+        # TODO: implement!
 
 
 class PyroScopeScreenlet(screenlets.Screenlet):
@@ -73,6 +184,8 @@ class PyroScopeScreenlet(screenlets.Screenlet):
                 self.add_option(opt_type(group, attr, getattr(self, attr), label, tooltip, **opt_kw)) 
 
         # Initial update
+        # TODO: connection URL must be an option
+        self.state = RtorrentState("scgi:///var/torrent/.scgi_local")
         self.set_update_interval()
         self.update()
 
@@ -98,7 +211,8 @@ class PyroScopeScreenlet(screenlets.Screenlet):
 
 
     def update(self):
-        self._content = datetime.datetime.fromtimestamp(time.time()).isoformat(' ')[:19]
+        self.state.update()
+        self._content = formatting.format_item(self.state.TEMPL, self.state)
         self.redraw_canvas()
         return True
 
@@ -120,10 +234,11 @@ class PyroScopeScreenlet(screenlets.Screenlet):
             shadow_size=6, shadow_color=self.shadow_color)
         #self.theme["background.svg"].render_cairo(ctx)
 
-        # compute space between fields
+        # render text
         ctx.set_source_rgba(*self.text_color)
-        self.draw_text(ctx, self._content, 10,10,  "FreeSans",10, self.width-20,
-            allignment=pango.ALIGN_LEFT,justify = True, weight=0)
+        # TODO: make font an option
+        self.draw_text(ctx, self._content, 10,10,  "DejaVu Sans Mono",9, self.width-20,
+            allignment=pango.ALIGN_LEFT, justify=True, weight=0)
 
     
     def on_draw_shape(self, ctx):
@@ -132,7 +247,7 @@ class PyroScopeScreenlet(screenlets.Screenlet):
 
 def run():
     """ If the program is run directly or passed as an argument to the python
-        interpreter then create a Screenlet instance and show it.
+        interpreter, then create a Screenlet instance and show it.
     """
     from screenlets import session
     session.create_session(PyroScopeScreenlet)
