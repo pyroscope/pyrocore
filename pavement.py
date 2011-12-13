25eaf269688c0f9814be8f0d5969f0656bed06cb
pyroscope <pyroscope.project@gmail.com>
2011-12-13 21:45:16 +0000

    added 'wiki' task to generate CliUsage page

--- pavement.py
+++ pavement.py
@@ -157,6 +157,40 @@ def build():
 
 
 @task
+def wiki():
+    "create some wiki pages automatically"
+    helppage = path("wiki/CliUsage.wiki")
+    content = [
+        "#summary PyroScope CLI Tools Usage.",
+        "#labels cli,shell",
+        '<wiki:toc max_depth="2" />',
+        "",
+        "This page is automatically generated and shows the options available in the *development* version of the code (SVN head).",
+        "See CommandLineTools for more details on how to use these commands.",
+        "The help output presented here applies to version %s of the tools." % sh("pyroadmin --version", capture=True).split()[1],
+        "",
+    ]
+    
+    for tool in sorted(project.entry_points["console_scripts"]):
+        tool, _ = tool.split(None, 1)
+        content.extend([
+            "= %s =" % tool,
+            "{{{",
+        ])
+        help_opt = "--help-fields --config-dir /tmp" if tool == "rtcontrol" else "--help"
+        content.extend(sh("%s -q %s" % (tool, help_opt), capture=True, ignore_error=True).splitlines())
+        content.extend([
+            "}}}",
+        ])
+
+    content = [line for line in content if all(
+        i not in line for i in (", Copyright (c) ", "Total time: ", "Configuration file '/tmp/")
+    )]
+    content = [line for line, succ in zip(content, content[1:] + ['']) if line or succ] # filter twin empty lines
+    helppage.write_lines(content)
+
+
+@task
 @needs("docs")
 def dist_docs():
     "create a documentation bundle"
