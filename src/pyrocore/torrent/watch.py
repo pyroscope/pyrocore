e71ff2cf61ba042a8c3bb1ea96f2911df33c4dd1
pyroscope <pyroscope.project@gmail.com>
2012-03-02 05:49:22 +0000

    tree watch basically working, with logging of writes and moves

--- src/pyrocore/torrent/watch.py
+++ src/pyrocore/torrent/watch.py
@@ -19,8 +19,19 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 from __future__ import with_statement
 
+import sys
+import logging
+import asyncore
+
+from pyrobase.parts import Bunch
 from pyrocore import error, config
 from pyrocore.util import os, xmlrpc, pymagic
+from pyrocore.scripts.base import ScriptBase, ScriptBaseWithConfig
+
+try: 
+    import pyinotify
+except ImportError, exc:
+    pyinotify = Bunch(WatchManager=None, ProcessEvent=object)
 
 
 class RemoteWatch(object):
@@ -44,7 +55,118 @@ class RemoteWatch(object):
         #   target      URL of target to watch
 
 
-class TreeWatch(object):
-    """ rTorrent folder tree watch via libnotify.
+class TreeWatchHandler(pyinotify.ProcessEvent):
+    """ inotify event handler for rTorrent folder tree watch.
+    
+        See https://github.com/seb-m/pyinotify/.
     """
+
+    def my_init(self, **kw):
+        self.job = kw["job"]
+        
+
+    def process_IN_CLOSE_WRITE(self, event):
+        """ File written.
+        """
+        # <Event dir=False name=xx path=/var/torrent/watch/tmp pathname=/var/torrent/watch/tmp/xx>
+        if not event.dir:
+            self.job.LOG.info("File %r written" % event.pathname)
+
+
+    def process_IN_MOVED_TO(self, event):
+        """ File moved into tree.
+        """
+        # <Event dir=False name=yy path=/var/torrent/watch/tmp pathname=/var/torrent/watch/tmp/yy>
+        if not event.dir:
+            self.job.LOG.info("File %r moved here" % event.pathname)
+
+
+    def process_default(self, event):
+        """ Fallback.
+        """
+        self.job.LOG.warning("Unexpected inotify event %r" % event)
+
+
+class TreeWatch(object):
+    """ rTorrent folder tree watch via inotify.
+    """
+
+    def __init__(self, config=None):
+        self.config = config or {}
+        self.LOG = pymagic.get_class_logger(self)
+        self.LOG.info("Tree watcher created with config %r" % self.config)
+
+        self.manager = None
+        self.handler = None
+        self.notifier = None
+
+        if not self.config.path:
+            raise error.UserError("You nedd to set 'job.%s.path' in the condfiguration!" % self.config.job_name)
+
+        self.config.path = os.path.abspath(os.path.expanduser(self.config.path.rstrip(os.sep)))
+        if not os.path.isdir(self.config.path):
+            raise error.UserError("Path '%s' is not a directory!" % self.config.path)
+        
+        if self.config.active:
+            self.setup()
+
+        
+    def setup(self):
+        """ Set up inotify manager.
+        
+            See https://github.com/seb-m/pyinotify/.
+        """
+        if not pyinotify.WatchManager:
+            raise error.UserError("You need to install 'pyinotify' to use %s!" % (
+                self.__class__.__name__))
+
+        self.manager = pyinotify.WatchManager()
+        self.handler = TreeWatchHandler(job=self)
+        self.notifier = pyinotify.AsyncNotifier(self.manager, self.handler)
+
+        mask = pyinotify.IN_CLOSE_WRITE | pyinotify.IN_MOVED_TO
+        self.manager.add_watch(self.config.path, mask, rec=True)
+
+
+    def run(self):
+        """ We don't really need any timed scheduling.
+        """
+        # TODO: Maybe do some stats logging here, once per hour or so
+        # XXX: We can handle files that were not valid bencode here, from a Queue! And watch.ini reloading.
+
+
+class TreeWatchCommand(ScriptBaseWithConfig):
+    ### Keep things wrapped to fit under this comment... ##############################
+    """ Use tree watcher directly from cmd line (python -m pyrocore.torrent.watch <DIR>)
+    """
+
+    # log level for user-visible standard logging
+    STD_LOG_LEVEL = logging.DEBUG
+
+    # argument description for the usage information
+    ARGS_HELP = "<directory>"
+
+
+    def mainloop(self):
+        """ The main loop.
+        """
+        # Print usage if not enough args or bad options
+        if len(self.args) < 1:
+            self.parser.error("No method given!")
+
+        watch = TreeWatch(Bunch(path = self.args[0], active = False, dry_run=True))
+        watch.setup()
+        asyncore.loop(timeout=~0, use_poll=True)
+
+
+    @classmethod
+    def main(cls): #pragma: no cover
+        """ The entry point.
+        """
+        ScriptBase.setup()
+        cls().run()
+
+
+if __name__ == "__main__":
+    TreeWatchCommand.main()
 
