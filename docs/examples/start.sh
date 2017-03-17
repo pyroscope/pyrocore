b111d95db7b591b9b42c8c4370abf5433575cf70
pyroscope <pyroscope.project@gmail.com>
2017-03-17 01:36:25 +0100

    start: sync with p-m-b, and added -D -I

--- docs/examples/start.sh
+++ docs/examples/start.sh
@@ -2,12 +2,22 @@
 #
 # rTorrent startup script
 #
+
+RT_BINDIR="{{ rtorrent_bindir }}"
+RT_OPTS=( )
+RT_OPTS+=( -D -I )  # comment this to get deprecated commands
+
 if [ "$TERM" = "${TERM%-256color}" ]; then
     export TERM="$TERM-256color"
 fi
+
 export LANG=en_US.UTF-8
 umask 0027
-cd $(dirname $0)
+
+builtin cd $(dirname $0)
+export RT_HOME="$PWD"
+RT_OPTS+=( -n -o "import=$RT_HOME/rtorrent.rc" )
+
 export RT_SOCKET=$PWD/.scgi_local
 test -S $RT_SOCKET && lsof $RT_SOCKET >/dev/null && { echo "rTorrent already running"; exit 1; }
 test ! -e $RT_SOCKET || rm $RT_SOCKET
@@ -18,6 +28,18 @@ _at_exit() {
     test ! -e $RT_SOCKET || rm $RT_SOCKET
 }
 trap _at_exit INT TERM EXIT
-test -z "$TMUX" || tmux rename-w rT-PS
+test -z "$TMUX" || tmux 'rename-w' 'rT-PS'
 
-rtorrent -n -o import=$PWD/rtorrent.rc
+if test -n "$RT_BINDIR"; then
+    RT_BINDIR="${RT_BINDIR%/}/"
+
+    # Try usual suspects if config is wrong
+    test -x ${RT_BINDIR}rtorrent || RT_BINDIR="$HOME/bin/"
+    test -x ${RT_BINDIR}rtorrent || RT_BINDIR="/opt/rtorrent/bin/"
+    test -x ${RT_BINDIR}rtorrent || RT_BINDIR="/usr/local/bin/"
+    test -x ${RT_BINDIR}rtorrent || RT_BINDIR="/usr/bin/"
+    test -x ${RT_BINDIR}rtorrent || RT_BINDIR=""
+fi
+#RT_BINDIR="$HOME/src/rtorrent-ps/rtorrent-0.9.6/src/"
+export RT_BIN=${RT_BINDIR}rtorrent
+$RT_BIN "${RT_OPTS[@]}"
