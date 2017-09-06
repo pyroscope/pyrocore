2dce8b3b42619f1d814f0cc78237d736721ccb99
kannibalox <kannibalox@gmail.com>
2017-09-06 23:03:57 +0000

    Overhaul and update InfluxDB engine

--- src/pyrocore/util/stats.py
+++ src/pyrocore/util/stats.py
@@ -19,36 +19,59 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 from __future__ import absolute_import
 
-import time
-
+from pyrocore import error
+from pyrocore.util import xmlrpc
 
 def engine_data(engine):
     """ Get important performance data and metadata from rTorrent.
     """
-    views = ("default", "main", "started", "stopped", "complete",
-             "incomplete", "seeding", "leeching", "active", "messages")
+    proxy = engine.open()
+    views = proxy.view.list()
+
     methods = [
         "throttle.global_up.rate", "throttle.global_up.max_rate",
         "throttle.global_down.rate", "throttle.global_down.max_rate",
+        "pieces.stats_not_preloaded", "pieces.stats_preloaded",
+        "system.files.opened_counter", "system.files.failed_counter", "system.files.closed_counter",
+        "pieces.memory.block_count", "pieces.memory.current",
+        "network.open_sockets"
     ]
 
     # Get data via multicall
-    proxy = engine.open()
     calls = [dict(methodName=method, params=[]) for method in methods] \
           + [dict(methodName="view.size", params=['', view]) for view in views]
     result = proxy.system.multicall(calls, flatten=True)
+    result_dict = {}
+    for m in methods:
+        result_dict[m] = result[0]
+        del result[0]
+    result_dict['views'] = {}
+    for v in views:
+        result_dict['views'][v] = {}
+        result_dict['views'][v]['size'] = result[0]
+        del result[0]
+    return result_dict
 
-    # Build result object
-    data = dict(
-        now         = time.time(),
-        engine_id   = engine.engine_id,
-        versions    = engine.versions,
-        uptime      = engine.uptime,
-        upload      = [result[0], result[1]],
-        download    = [result[2], result[3]],
-        views       = dict([(name, result[4+i])
-            for i, name in enumerate(views)
-        ]),
-    )
+def module_test():
+    """ Quick test using…
 
-    return data
+            python -m pyrocore.util.stats
+    """
+    import pprint
+    from pyrocore import connect
+
+    try:
+        engine = connect()
+        print("%s - %s" % (engine.engine_id, engine.open()))
+
+        result = engine_data(engine)
+        print "result = ",
+        pprint.pprint(result)
+
+        print("%s - %s" % (engine.engine_id, engine.open()))
+    except (error.LoggableError, xmlrpc.ERRORS), torrent_exc:
+        print("ERROR: %s" % torrent_exc)
+
+
+if __name__ == "__main__":
+    module_test()
