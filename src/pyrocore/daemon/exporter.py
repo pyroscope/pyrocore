88ed6cc4f3f4b8e5bd0b22d24134cb18ae86187c
kannibalox <kannibalox@gmail.com>
2018-11-10 00:48:43 +0000

    Use the python prometheus client appropriately
    Previously the exporter assumed it was rtorrent's main process
    and would continue to server the same stats

--- src/pyrocore/daemon/exporter.py
+++ src/pyrocore/daemon/exporter.py
@@ -7,6 +7,7 @@ from pyrocore.util import pymagic
 from pyrobase.parts import Bunch
 
 from prometheus_client import start_http_server, Gauge
+from prometheus_client.core import GaugeMetricFamily, REGISTRY
 
 class ClientServer(threading.Thread):
     def __init__(self, port):
@@ -16,103 +17,72 @@ class ClientServer(threading.Thread):
     def run(self):
         start_http_server(self.port)
 
-class RtorrentExporter(object):
-    """ Expose rTorrent and host statistics for scraping by a Prometheus instance.
-    """
-
-    def __init__(self, config=None):
-        """ Set up RtorrentExporter.
-        """
-        self.config = config or Bunch()
-        self.LOG = pymagic.get_class_logger(self)
-        if 'log_level' in self.config:
-            self.LOG.setLevel(config.log_level)
-        self.LOG.debug("RtorrentExporter created with config %r" % self.config)
+class RtorrentCollector(object):
+    def __init__(self, proxy, config):
+        self.proxy = proxy
+        self.config = config
         self.prefix = self.config.get('prefix', 'rtorrent_')
-        self.proxy = config_ini.engine.open()
-        self.jobs = []
-        jobs_init = {
-            'tracker': self._init_tracker_stats,
-            'system': self._init_system_stats,
-            'item': self._init_item_stats
-        }
-        for j in self.config.get('jobs', 'system').split(','):
-            if j in ['tracker', 'system', 'item']:
-                self.jobs.append(j)
-                jobs_init[j]()
-            else:
-                self.LOG.error("Unknown job '{}' requested, not initializing it".format(j))
-        if not self.jobs:
-            raise RuntimeError("Job configuration '{}' contained no valid jobs".format(self.config.get('jobs')))
-        # Start the server right off the bat
-        self.prom_thread = ClientServer(self.config.get('port', '8000'))
-        self.prom_thread.start()
 
 
-    def run(self):
-        """Update any defined metrics
-        """
-        # Update requested stats
-        jobs = {
-            'tracker': self._fetch_tracker_stats,
-            'system': self._fetch_system_stats,
-            'item': self._fetch_item_stats
-        }
-        for j in self.jobs:
-            jobs[j]()
+    def collect(self):
+        raise NotImplementedError
 
-    def _init_item_stats(self):
+class RtorrentItemCollector(RtorrentCollector):
+    def __init__(self, proxy, config):
+        super(RtorrentItemCollector, self).__init__(proxy, config)
+
         available_methods = set(self.proxy.system.listMethods())
-        if 'item_stats' in self.config:
-            item_stat_methods = self.config['item_stats'].split(',')
-            item_stat_methods = set(item_stat_methods) & available_methods
+        if 'item-stats' in self.config:
+            self.item_stat_methods = set(self.config['item-stats'].split(',')) & available_methods
         else:
-            item_stat_methods = ("down.total", "up.total")
-        if 'item_labels' in self.config:
-            item_labels = self.config['item_labels'].split(',')
-            self.item_labels = list(set(item_labels) & available_methods)
+            self.item_stat_methods = ("down.total", "up.total")
+        if 'item-labels' in self.config:
+            self.item_labels = list(set(self.config['item-labels'].split(',')) & available_methods)
         else:
             self.item_labels = ["hash", "name"]
-        self.item_stats = {}
-        for m in item_stat_methods:
-            self.item_stats[m] = Gauge(self.prefix + "item_" + m.replace('.', '_'), m, self.item_labels)
 
-    def _fetch_item_stats(self):
-        """Use d.multicall2 to
-        """
-        calls = ["d."+m+"=" for m in list(self.item_stats.keys()) + self.item_labels]
-        result = self.proxy.d.multicall2('', "main", *calls)
+
+    def collect(self):
+        calls = ["d."+m+"=" for m in list(self.item_stat_methods) + self.item_labels]
+        result = self.proxy.d.multicall("main", *calls)
+        item_stats = {}
+        for stat in self.item_stat_methods:
+            item_stats[stat] = GaugeMetricFamily(self.prefix + stat.replace('.', '_'), stat, labels=self.item_labels)
         for i in result:
-            info = dict(list(zip(list(self.item_stats.keys()) + self.item_labels, i)))
-            for stat, gauge in self.item_stats.items():
-                gauge.labels(*[info[l] for l in self.item_labels]).set(info[stat])
-
-    def _init_tracker_stats(self):
-        """Initialize the tracker gauges
-        """
-        self.tracker_gauge = Gauge(self.prefix + 'tracker_amount', 'Number of torrents belonging to a specific tracker', ['alias'])
-        self.tracker_error_gauge = Gauge(self.prefix + 'tracker_errors',
-                                         'Number of torrents with tracker errors belonging to a specific tracker', ['alias'])
+            info = dict(list(zip(list(self.item_stat_methods) + self.item_labels, i)))
+            for stat, gauge in item_stats.items():
+                gauge.add_metric([info[l] for l in self.item_labels], info[stat])
+        for stat, guage in item_stats.items():
+            yield guage
 
 
-    def _fetch_tracker_stats(self):
-        """Scrape tracker metrics from item information
-        """
+class RtorrentTrackerCollector(RtorrentCollector):
+    def __init__(self, proxy, config):
+        super(RtorrentTrackerCollector, self).__init__(proxy, config)
+
+    def collect(self):
+        tracker_gauge = GaugeMetricFamily(self.prefix + 'tracker_amount',
+                                          'Number of torrents belonging to a specific tracker', labels=['alias'])
+        tracker_error_gauge = GaugeMetricFamily(self.prefix + 'tracker_errors',
+                                                'Number of torrents with tracker errors belonging to a specific tracker', labels=['alias'])
+
         item_fields = ["d.tracker_domain=", "d.message="]
-
         result = self.proxy.d.multicall("main", *item_fields)
 
         trackers = Counter([config_ini.map_announce2alias(d[0]) for d in result])
         tracker_errors = Counter([config_ini.map_announce2alias(d[0]) for d in result if d[1]])
 
         for k, v in trackers.items():
-            self.tracker_gauge.labels(k).set(v)
+            tracker_gauge.add_metric([k], v)
         for k in trackers.keys(): # Use the tracker keys to make sure all active trackers get a value
-            self.tracker_error_gauge.labels(k).set(tracker_errors[k])
+            tracker_error_gauge.add_metric([k], tracker_errors[k])
 
-    def _init_system_stats(self):
-        """Initialize the system gauges
-        """
+        yield tracker_gauge
+        yield tracker_error_gauge
+
+class RtorrentSystemCollector(RtorrentCollector):
+    def __init__(self, proxy, config):
+        super(RtorrentSystemCollector, self).__init__(proxy, config)
         stat_methods = [
             "throttle.global_up.rate", "throttle.global_up.max_rate", "throttle.global_up.total",
             "throttle.global_down.rate", "throttle.global_down.max_rate", "throttle.global_down.total",
@@ -128,53 +98,82 @@ class RtorrentExporter(object):
             "startup_time", "network.http.current_open"
         ]
 
-        info_methods = ['system.client_version', 'system.library_version']
+        self.info_methods = ['system.client_version', 'system.library_version']
 
-        self.system_stats = {}
-        for m in set(stat_methods) & set(self.proxy.system.listMethods()): # Strip out any methods that aren't available on the system
-            self.system_stats[m] = Gauge(self.prefix + m.replace('.', '_'), m)
-        self.system_info = Gauge(self.prefix + "info", "rTorrent platform information", [m.replace('.','_') for m in info_methods])
-        self.system_view_size = Gauge(self.prefix + "view_size", "Size of rtorrent views", ["view"])
+        # Strip out unavailable methods
+        self.system_stats = set(stat_methods) & set(self.proxy.system.listMethods())
 
-    def _fetch_system_stats(self):
-        """Scrape system and view statistics
-        """
-        info_methods = ['system.client_version', 'system.library_version']
+    def collect(self):
+        system_info = GaugeMetricFamily(self.prefix + "info", "rTorrent platform information", labels=[m.replace('.','_') for m in self.info_methods])
+        system_view_size = GaugeMetricFamily(self.prefix + "view_size", "Size of rtorrent views", labels=["view"])
         views = self.proxy.view.list()
 
         # Get data via multicall
-        # Sort the system stats because we can't trust py2 keys() to be deterministic
-        calls = [dict(methodName=method, params=[]) for method in sorted(self.system_stats.keys())] \
-                + [dict(methodName=method, params=[]) for method in info_methods] \
+        calls = [dict(methodName=method, params=[]) for method in sorted(self.system_stats)] \
+                + [dict(methodName=method, params=[]) for method in self.info_methods] \
                 + [dict(methodName="view.size", params=['', view]) for view in views]
 
         result = self.proxy.system.multicall(calls, flatten=True)
 
         # Get numeric metrics
-        for m in sorted(self.system_stats.keys()):
-            self.system_stats[m].set(result[0])
+        for m in sorted(self.system_stats):
+            yield GaugeMetricFamily(self.prefix + m.replace('.', '_'), m, value=result[0])
             del result[0]
 
         # Get text-like information
-        info_methods = [m.replace('.', '_') for m in info_methods]
-        self.system_info.labels(*result[0:len(result)-len(views)]).set(1)
+        info_methods = [m.replace('.', '_') for m in self.info_methods]
+        system_info.add_metric(result[0:len(result)-len(views)], 1)
+        yield system_info
         result = result[-len(views):]
 
         # Get view information
         for v in views:
-            self.system_view_size.labels(v).set(result[0])
+            system_view_size.add_metric([v], result[0])
             del result[0]
 
 
-def module_test():
+class RtorrentExporter(object):
+    """ Expose rTorrent and host statistics for scraping by a Prometheus instance.
+    """
+
+    def __init__(self, config=None):
+        """ Set up RtorrentExporter.
+        """
+        self.config = config or Bunch()
+        self.LOG = pymagic.get_class_logger(self)
+        if 'log_level' in self.config:
+            self.LOG.setLevel(config.log_level)
+        self.LOG.debug("RtorrentExporter created with config %r" % self.config)
+        self.prefix = self.config.get('prefix', 'rtorrent_')
+        self.proxy = config_ini.engine.open()
+        self.system_stats_initialized = False
+        jobs = {
+            "item": RtorrentItemCollector,
+            "tracker": RtorrentTrackerCollector,
+            "system": RtorrentSystemCollector
+        }
+        for j in self.config.get('jobs', 'system').split(','):
+             j = j.strip()
+             if j not in jobs:
+                 self.LOG.error("Job {} not found, skipping".format(j))
+             else:
+                 REGISTRY.register(jobs[j](self.proxy, self.config))
+
+        # Start the server right off the bat
+        self.prom_thread = ClientServer(self.config.get('port', '8000'))
+        self.prom_thread.start()
+
+
+    def run(self):
+        # NOOP, stats are generated at scrape time
+        pass
+
+if __name__ == '__main__':
     from pyrocore import connect
     engine = connect()
 
-    i = RtorrentExporter(Bunch(jobs="system,tracker,item",port="8100"))
+    i = RtorrentExporter(Bunch(jobs="system,tracker,item", port=8005))
     i.proxy = engine.open()
     while True:
         i.run()
         sleep(5)
-
-if __name__ == '__main__':
-    module_test()
